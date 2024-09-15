513848bfafff8386bddb8295c4ab60ec4d03be26
HorrorEmpire <113548417+HorrorEmpire@users.noreply.github.com>
2024-09-15 18:26:13 +0200

    Restructure: Typescript --> Javascript

---
+++ nextfrontend/app/projects/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { Card, Text } from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <main>
+      <h2 className="Hocn">Hockn</h2>
+      <Card></Card>
+    </main>
+  );
+}
