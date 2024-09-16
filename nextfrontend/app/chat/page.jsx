626505a34eb0135aa8422ce4d750170fce5bfbfe
HorrorEmpire <113548417+HorrorEmpire@users.noreply.github.com>
2024-09-16 09:10:02 +0200

    ChatComponents added

---
+++ nextfrontend/app/chat/page.jsx
@@ -0,0 +1,11 @@
+import ChatList from '../lists/ChatList';
+
+const ChatPage = () => {
+  return (
+      <div className="ml-2 mt-0.125 p-4">
+        <ChatList />
+      </div>
+  );
+};
+
+export default ChatPage;
---
+++ nextfrontend/app/lists/ChatList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ChatCard from '../components/ChatCard';
+
+import teams from '@/Data/data';
+
+/**
+  This list goes through the data, and creates a TeamCard 
+  with the data
+*/
+const TeamList = () => {
+  return (
+    <div style={listStyle}>
+      {teams.map((team, index) => (
+        <ChatCard
+
+        />
+      ))}
+    </div>
+  );
+};
+
+/**
+  some basic CSS-formatation for the List:
+  - display flex for listing them in a row
+  - flexWrap for going to the next row
+  - justifyContent flex-start for putting it on 
+    the left side
+*/
+const listStyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around',
+  justifyContent: 'flex-start',
+};
+
+export default TeamList;
