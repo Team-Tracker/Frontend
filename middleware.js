fdbbbc60b87af89da2432ee22a085ed3b8ab71f4
Lorenz Geyser <lorenz.geyser@outlook.com>
2025-02-27 12:03:42 +0100

    Google

--- middleware.js
+++ middleware.js
@@ -1,40 +1,14 @@
 import { NextResponse } from 'next/server'
-// import { decrypt } from '@/app/lib/session'
-// import { cookies } from 'next/headers'
- 
-// 1. Specify protected and public routes
+
 const protectedRoutes = ['/', '/chat*', '/projects*', '/teams*']
 const publicRoutes = ['/login']
- 
-// export default async function middleware(req) {
-//   // 2. Check if the current route is protected or public
-//   const path = req.nextUrl.pathname
-//   const isProtectedRoute = protectedRoutes.includes(path)
-//   const isPublicRoute = publicRoutes.includes(path)
- 
-//   // 3. Decrypt the session from the cookie
-//   const cookie = cookies().get('session')?.value
-//   const session = await decrypt(cookie)
- 
-//   // 5. Redirect to /login if the user is not authenticated
-//   if (isProtectedRoute && !session?.userId) {
-//     return NextResponse.redirect(new URL('/login', req.nextUrl))
-//   }
- 
-//   // 6. Redirect to /dashboard if the user is authenticated
-//   if (
-//     isPublicRoute &&
-//     session?.userId &&
-//     !req.nextUrl.pathname.startsWith('/login')
-//   ) {
-//     return NextResponse.redirect(new URL('/', req.nextUrl))
-//   }
- 
-//   return NextResponse.next()
-// }
 
 export function middleware(request) {
   const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith('/google') || pathname.startsWith('/bing')) {
+    return NextResponse.next();
+  }
 
   // Skip middleware for public routes
   if (publicRoutes.includes(pathname)) {
@@ -49,17 +23,16 @@ export function middleware(request) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  // If username exists and user tries to get to "/login" redirect
-  // to home
-  if(username && pathname === '/login') {
-    return NextResponse.redirect(new URL('/teams', request.url))
+  // If username exists and user tries to get to "/login", redirect to home
+  if (username && pathname === '/login') {
+    return NextResponse.redirect(new URL('/teams', request.url));
   }
 
-  // If user is logged in, continue with the request
   return NextResponse.next();
 }
 
-// Routes Middleware should not run on
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-}
+  matcher: [
+    '/((?!api|_next/static|_next/image|.*\\.png$|.*\\.jpg$|.*\\.svg$|google.*\\.html|bing.*\\.html).*)',
+  ],
+};
